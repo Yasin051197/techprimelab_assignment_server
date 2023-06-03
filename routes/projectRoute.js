758d9dbffac6dc563d840f18d785a987f47986fe
Yasin051197 <yasinjamadar0511@gmail.com>
2023-06-03 13:25:08 +0530

    added edit routes

--- routes/projectRoute.js
+++ routes/projectRoute.js
@@ -18,4 +18,32 @@ projectRoute.post("/createproject",async(req,res)=>{
     }
 })
 
+projectRoute.get("/projects",async(req,res)=>{
+    try {
+        const projects = await Project.find();
+        if (!projects) {
+          res.send({ msg: 'project not found' });
+        } else {
+          res.send(projects);
+        }
+      } catch (err) {
+        res.send({msg:"Error in finding projects"});
+      }
+})
+projectRoute.put("/projects/:id",async(req,res)=>{
+    const id=req.params.id;
+  try {
+    const project = await Project.findById(id);
+    if (!project) {
+      res.send({ msg: 'project not found' });
+    } else {
+        const {status}=req.body
+       project.status= status
+      await project.save();
+      res.send({ msg: 'project updated successfully', project: project });
+    }
+  } catch (err) {
+    res.send({msg:"Error in updating users data,try again"});
+  }
+})
 module.exports={projectRoute};
